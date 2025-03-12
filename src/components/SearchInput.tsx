173631a981a24c8cecba6bd13b180cb0420d57c2
Lim Ting Zhi <limtingzhi@gmail.com>
2025-03-12 15:16:31 +0800

    feat: style SearchInput components

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -1,4 +1,7 @@
 import { useCallback } from 'react';
+import ClearIcon from './Icons/ClearIcon';
+import SearchIcon from './Icons/SearchIcon';
+import './SearchInput.css';
 
 interface Props {
   city: string;
@@ -21,29 +24,59 @@ const SearchInput = (props: Props) => {
   }, [setCity, setCountry]);
 
   return (
-    <div>
-      <input
-        autoComplete="off"
-        name="city"
-        onChange={e => setCity(e.target.value)}
-        placeholder="City"
-        type="text"
-        value={city}
-      />
-      <input
-        autoComplete="off"
-        name="country"
-        onChange={e => setCountry(e.target.value)}
-        placeholder="Country"
-        type="text"
-        value={country}
-      />
-      <button onClick={submit} type="submit">
-        Search
-      </button>
-      <button onClick={clearInputs} type="reset">
-        Clear
-      </button>
+    <div className="search-input">
+      <div className="search-input__inputs">
+        <div className="search-input__group">
+          <input
+            autoComplete="off"
+            className="search-input__input"
+            id="city"
+            onChange={e => setCity(e.target.value)}
+            placeholder=""
+            type="text"
+            value={city}
+          />
+          <label
+            className="search-input__label"
+            htmlFor="city"
+          >
+            City
+          </label>
+        </div>
+        <div className="search-input__group">
+          <input
+            autoComplete="off"
+            className="search-input__input"
+            id="country"
+            onChange={e => setCountry(e.target.value)}
+            placeholder=""
+            type="text"
+            value={country}
+          />
+          <label
+            className="search-input__label"
+            htmlFor="country"
+          >
+            Country
+          </label>
+        </div>
+      </div>
+      <div className="search-input__btns">
+        <button
+          className="search-input__btn"
+          onClick={submit}
+          type="submit"
+        >
+          <SearchIcon className="search-input__btn-icon" />
+        </button>
+        <button
+          className="search-input__btn"
+          onClick={clearInputs}
+          type="reset"
+        >
+          <ClearIcon className="search-input__btn-icon" />
+        </button>
+      </div>
     </div>
   );
 };
