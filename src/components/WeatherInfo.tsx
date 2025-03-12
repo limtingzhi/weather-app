cd06f629a503c75c6d1b5b903e87d68a48d0857b
Lim Ting Zhi <limtingzhi@gmail.com>
2025-03-13 00:33:15 +0800

    feat: add desktop styles

--- src/components/WeatherInfo.tsx
+++ src/components/WeatherInfo.tsx
@@ -21,12 +21,22 @@ const WeatherInfo = (props: Props) => {
 
   return (
     <div className="weather-info">
-      <img alt="weather" className="weather-info__img" src={weatherImage} />
+      <img
+        alt="weather"
+        className="weather-info__img"
+        src={weatherImage}
+      />
       <div className="weather-info__left">
         <div>Today’s Weather</div>
-        <div className="weather-info__temp">{`${Math.round(weatherInfo.temp)}°`}</div>
-        <div>{`H: ${Math.round(weatherInfo.maxTemp)}° L: ${Math.round(weatherInfo.minTemp)}°`}</div>
-        <div className="weather-info__location">{`${weatherInfo.city}, ${weatherInfo.country}`}</div>
+        <div className="weather-info__temp">
+          {`${Math.round(weatherInfo.temp)}°`}
+        </div>
+        <div>
+          {`H: ${Math.round(weatherInfo.maxTemp)}° L: ${Math.round(weatherInfo.minTemp)}°`}
+        </div>
+        <div className="weather-info__location">
+          {`${weatherInfo.city}, ${weatherInfo.country}`}
+        </div>
       </div>
       <div className="weather-info__right">
         <div>{weatherInfo.weather}</div>
